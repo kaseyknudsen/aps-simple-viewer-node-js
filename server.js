4b1842770768c9ac7dbf59a4fb2598751efb7f4f
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-05-31 23:04:32 -0700

    added oss logic

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = require('express');
-const { PORT } = require('./config.js');
+const express = require("express");
+const { PORT } = require("./config.js");
 
 let app = express();
-app.use(express.static('wwwroot'));
-app.listen(PORT, function () { console.log(`Server listening on port ${PORT}...`); });
+app.use(express.static("wwwroot"));
+app.use(require("./routes/auth.js"));
+app.listen(PORT, function () {
+  console.log(`Server listening on port ${PORT}...`);
+});
--- services/aps.js
+++ services/aps.js
@@ -1,22 +1,98 @@
-const fs = require('fs');
-const APS = require('forge-apis');
-const { APS_CLIENT_ID, APS_CLIENT_SECRET, APS_BUCKET } = require('../config.js');
+const fs = require("fs");
+const APS = require("forge-apis");
+const {
+  APS_CLIENT_ID,
+  APS_CLIENT_SECRET,
+  APS_BUCKET,
+} = require("../config.js");
 
-let internalAuthClient = new APS.AuthClientTwoLegged(APS_CLIENT_ID, APS_CLIENT_SECRET, ['bucket:read', 'bucket:create', 'data:read', 'data:write', 'data:create'], true);
-let publicAuthClient = new APS.AuthClientTwoLegged(APS_CLIENT_ID, APS_CLIENT_SECRET, ['viewables:read'], true);
+let internalAuthClient = new APS.AuthClientTwoLegged(
+  APS_CLIENT_ID,
+  APS_CLIENT_SECRET,
+  ["bucket:read", "bucket:create", "data:read", "data:write", "data:create"],
+  true
+);
+let publicAuthClient = new APS.AuthClientTwoLegged(
+  APS_CLIENT_ID,
+  APS_CLIENT_SECRET,
+  ["viewables:read"],
+  true
+);
 
-const service = module.exports = {};
+const service = (module.exports = {});
 
 service.getInternalToken = async () => {
-    if (!internalAuthClient.isAuthorized()) {
-        await internalAuthClient.authenticate();
-    }
-    return internalAuthClient.getCredentials();
+  if (!internalAuthClient.isAuthorized()) {
+    await internalAuthClient.authenticate();
+  }
+  return internalAuthClient.getCredentials();
 };
 
 service.getPublicToken = async () => {
-    if (!publicAuthClient.isAuthorized()) {
-        await publicAuthClient.authenticate();
+  if (!publicAuthClient.isAuthorized()) {
+    await publicAuthClient.authenticate();
+  }
+  return publicAuthClient.getCredentials();
+};
+
+//object storage service logic
+
+service.ensureBucketExists = async (bucketKey) => {
+  try {
+    await new APS.BucketsApi().getBucketDetails(
+      bucketKey,
+      null,
+      await service.getInternalToken()
+    );
+  } catch (err) {
+    if (err.response.status === 404) {
+      await new APS.BucketsApi().createBucket(
+        { bucketKey, policyKey: "persistent" },
+        {},
+        null,
+        await service.getInternalToken()
+      );
+    } else {
+      throw err;
     }
-    return publicAuthClient.getCredentials();
+  }
+};
+
+service.listObjects = async () => {
+  await service.ensureBucketExists(APS_BUCKET);
+  let resp = await new APS.ObjectsApi().getObjects(
+    APS_BUCKET,
+    { limit: 64 },
+    null,
+    await service.getInternalToken()
+  );
+  let objects = resp.body.items;
+  while (resp.body.next) {
+    const startAt = new URL(resp.body.next).searchParams.get("startAt");
+    resp = await new APS.ObjectsApi().getObjects(
+      APS_BUCKET,
+      { limit: 64, startAt },
+      null,
+      await service.getInternalToken()
+    );
+    objects = objects.concat(resp.body.items);
+  }
+  return objects;
+};
+
+service.uploadObject = async (objectName, filePath) => {
+  await service.ensureBucketExists(APS_BUCKET);
+  const buffer = await fs.promises.readFile(filePath);
+  const results = await new APS.ObjectsApi().uploadResources(
+    APS_BUCKET,
+    [{ objectKey: objectName, data: buffer }],
+    { useAcceleration: false, minutesExpiration: 15 },
+    null,
+    await service.getInternalToken()
+  );
+  if (results[0].error) {
+    throw results[0].completed;
+  } else {
+    return results[0].completed;
+  }
 };
